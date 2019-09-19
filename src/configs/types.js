bcb9aea86d7126dbc1638ed52e5ada5728d322e9
Mateen Kazia <mateencode@gmail.com>
2019-09-18 17:58:49 -0700

    config and helper folder added

---
+++ src/configs/types.js
@@ -0,0 +1,8 @@
+export const PUBLIC_API_KEY = "";
+
+export const CREATE_TODO = "CREATE_TODO";
+export const DELETE_TODO = "DELETE_TODO";
+export const EDIT_TODO = "FETCH_TODO";
+export const PATCH_TODO = "PATCH_TODO";
+export const FETCH_TODO = "FETCH_TODO";
+export const FETCH_TODOS = "FETCH_TODOS";
